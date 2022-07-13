a1cd77e7a063b4d93eac1259cb6417fdc379395a
mayara karina <mayara karina>
2022-07-13 13:11:47 -0300

    first commit

---
+++ listaDeAlunos.js
@@ -0,0 +1,11 @@
+let numeroDeAlunos = 10
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    console.log(contador);
+ 
+
+    if (contador == 0) { 
+            console.log("numero igual a zero")  
+    }
+
+}
